c941a8646dfb2bd333b8adbb320c88ff8e0d1e5e
Jeel Dobariya <jeeldobariya38@gmail.com>
2023-10-14 18:22:18 +0530

    add doc strings

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
+"""Execution of application begins from this file"""
+
 def init():
+    """initializes the app and greet the user"""
     print("Welcome to Smart Manager Application")
     print()
 
 def main():
+    """handles execution of application"""
     pass
 
 if __name__ == "__main__":
