1eac18f9e3104f2828078153cc4a29e3ad23c717
Jeel Dobariya <136002730+JeelDobariya38@users.noreply.github.com>
2023-10-14 20:27:46 +0530

    Add a Simple Behaviour to Application (#4)
    * add a greeting logic.
    
    * add doc strings
    
    * add type hints
    
    * add a two line between function
    
    * improved the workflow
    
    * add a input handler class
    
    * add a quit or terminating behavior to app
    
    * add some testcases to test the changes
    
    * renamed mockinput to MockInputHandler class
    
    * add some case-insensitivity to code

---
+++ src/main.py
@@ -0,0 +1,20 @@
+from app.input_handler import InputHandler
+
+def init() -> None:
+    print("Welcome to Smart Manager Application")
+    print()
+
+def main(inputhandler: InputHandler) -> None:
+    while True:
+        inp = inputhandler.input(">>").strip().lower()
+        
+        if inp == "quit":
+            print("Quitting the app...")
+            return
+        
+        if inp != "":
+            print("Invalid Input!!!")
+
+if __name__ == "__main__":
+    init()
+    main(InputHandler())
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,2 +1,44 @@
-def test_demo():
-    assert "this is test demo" == "this is test demo"
+from main import init, main
+from mocking import MockInputHandler
+
+def test_init(capfd):
+    init()
+    captured = capfd.readouterr()
+    assert "Welcome to Smart Manager Application" in captured.out
+
+class TestMain:
+    def test_main_command_symbol(self, capfd) -> None:
+        mock = MockInputHandler(["quit"])
+        main(mock)
+        captured = capfd.readouterr()
+        assert ">>" in captured.out
+
+    def test_main_quit_command(self, capfd) -> None:
+        mock = MockInputHandler(["quit"])
+        main(mock)
+        captured = capfd.readouterr()
+        assert "Quitting the app..." in captured.out
+
+    def test_main_spaced_command(self, capfd) -> None:
+        mock = MockInputHandler(["  quit"])
+        main(mock)
+        captured = capfd.readouterr()
+        assert "Quitting the app..." in captured.out
+    
+    def test_main_uppercase_command(self, capfd) -> None:
+        mock = MockInputHandler(["QUIT"])
+        main(mock)
+        captured = capfd.readouterr()
+        assert "Quitting the app..." in captured.out
+
+    def test_main_empty_command(self, capfd) -> None:
+        mock = MockInputHandler(["quit","",""])
+        main(mock)
+        captured = capfd.readouterr()
+        assert ">>\n>>\nQuitting the app..." in captured.out
+
+    def test_main_Invalid_command(self, capfd) -> None:
+        mock = MockInputHandler(["quit","","invalid"])
+        main(mock)
+        captured = capfd.readouterr()
+        assert "Invalid Input!!!" in captured.out
