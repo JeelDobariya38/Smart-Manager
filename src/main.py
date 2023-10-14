979cdf629d0299bf761872b8da0ce2dd99d9a43e
Jeel Dobariya <jeeldobariya38@gmail.com>
2023-10-14 18:26:05 +0530

    add a two line between function

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 """Execution of application begins from this file"""
 
+
 def init() -> None:
     """initializes the app and greet the user"""
     print("Welcome to Smart Manager Application")
     print()
 
+
 def main() -> None:
     """handles execution of application"""
+
 
 if __name__ == "__main__":
     init()
