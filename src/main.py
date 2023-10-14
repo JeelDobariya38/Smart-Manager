3669b892cd14a116b829477f2f0374078c6a58cb
Jeel Dobariya <jeeldobariya38@gmail.com>
2023-10-14 19:09:11 +0530

    add a quit or terminating behavior to app

--- src/main.py
+++ src/main.py
@@ -1,16 +1,20 @@
-"""Execution of application begins from this file"""
-
+from app.input_handler import InputHandler
 
 def init() -> None:
-    """initializes the app and greet the user"""
     print("Welcome to Smart Manager Application")
     print()
 
-
-def main() -> None:
-    """handles execution of application"""
-
+def main(inputhandler: InputHandler) -> None:
+    while True:
+        inp = inputhandler.input(">>").strip()
+        
+        if inp == "quit":
+            print("Quitting the app...")
+            return
+        
+        if inp != "":
+            print("Invalid Input!!!")
 
 if __name__ == "__main__":
     init()
-    main()
+    main(InputHandler())
