3e1dea34efd1b2620b3b2e4a7bae7ec49480fede
Jeel Dobariya <jeeldobariya38@gmail.com>
2023-11-07 16:47:37 +0530

    add typehint

--- src/app/datahandler.py
+++ src/app/datahandler.py
@@ -1,13 +1,15 @@
 from . import metadata
 
-DATA_FILE = metadata.DATADIR_PATH + "data.txt"
+from typing import List
 
-def save_data(website, password):
+DATA_FILE: str = metadata.DATADIR_PATH + "data.txt"
+
+def save_data(website: str, password: str) -> None:
     data = metadata.DATA_SEPARATOR.join([website, password])
     with open(DATA_FILE, "a") as f:
         f.write(data + '\n')
 
-def load_data():
+def load_data() -> List[str]:
     data_list = []
     with open(DATA_FILE) as f:
         data = f.read()
--- src/app/metadata.py
+++ src/app/metadata.py
@@ -3,18 +3,18 @@
 import os
 
 # App
-NAME = "Smart Manager"
-VERSION = "0.1.0-alpha"
-WEBSITE = "https://jeeldobariya38.github.io/Smart-Manager/"
-REPO = "https://github.com/JeelDobariya38/Smart-Manager"
+NAME: str = "Smart Manager"
+VERSION: str = "0.1.0-alpha"
+WEBSITE: str = "https://jeeldobariya38.github.io/Smart-Manager/"
+REPO: str = "https://github.com/JeelDobariya38/Smart-Manager"
 
 # Paths (all path are relative to this file)
-__DATADIR = ["..", "..", "..", "data"]
-__RESOURCEDIR = ["..", "..", "..", "resource"]
+__DATADIR: str = ["..", "..", "..", "data"]
+__RESOURCEDIR: str = ["..", "..", "..", "resource"]
 
 # FilePath (Do Not Change this session, instead change above "Paths" session)
-DATADIR_PATH = os.path.join(__file__, os.path.sep.join(__DATADIR)) + os.path.sep
-RESOURCEDIR_PATH = os.path.join(__file__, os.path.sep.join(__RESOURCEDIR)) + os.path.sep
+DATADIR_PATH: str = os.path.join(__file__, os.path.sep.join(__DATADIR)) + os.path.sep
+RESOURCEDIR_PATH: str = os.path.join(__file__, os.path.sep.join(__RESOURCEDIR)) + os.path.sep
 
 # App Configuration
-DATA_SEPARATOR = " -> "
+DATA_SEPARATOR: str = " -> "
