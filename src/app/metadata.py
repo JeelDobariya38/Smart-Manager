1f9b317f3fa9fb2ac6e9598b47c3555b7e128b47
Jeel Dobariya <jeeldobariya38@gmail.com>
2023-11-07 14:35:02 +0530

    Create metadata.py

---
+++ src/app/metadata.py
@@ -0,0 +1,11 @@
+"""CHANGES IN THIS FILE CAN LEAD TO ADVERSE EFFECTS AND CAN EVEN CRASH THE APPLICATION"""
+
+# App
+NAME = "Smart Manager"
+VERSION = "0.1.0-alpha"
+WEBSITE = "https://jeeldobariya38.github.io/Smart-Manager/"
+REPO = "https://github.com/JeelDobariya38/Smart-Manager"
+
+# FilePath (all path are relative to src directory)
+DATADIR_PATH = "data"
+RESOURCE_PATH = "resource"
