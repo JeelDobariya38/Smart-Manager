58b6149fb53df66c24ec26b784370839d411a5b0
Jeel Dobariya <jeeldobariya38@gmail.com>
2023-12-01 21:15:58 +0530

    create a main.py

--- src/main.py
+++ src/main.py
@@ -1,2 +1,12 @@
-print("Version is Deprecated")
-input("please try other versions")
+from . import App
+
+import sys
+import os
+
+def resolve_path():
+    sys.path.insert(0, os.path.dirname(__file__))
+
+if __name__ == "__main__":
+    resolve_path()
+    app.init()
+    app.main()
