139d5b019630a9f6ca23dbccd21659ae3b153474
Jeel Dobariya <jeeldobariya38@gmail.com>
2023-12-16 23:03:34 +0530

    add a more testcases

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,12 @@
 from mocking import MockController
 
+
 def test_welcome_msg(SmartManger, capfd):
     SmartManger.welcome()
     out, _ = capfd.readouterr()
     assert "Welcome to Smart Manager Application" in out
+
+
+def test_app_exist(SmartManger):
+    SmartManger.init()
+    SmartManger.main()
