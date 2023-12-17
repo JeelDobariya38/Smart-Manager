a1a7af3034b2382bdafcdd894848cd5521bbbcd5
Jeel Dobariya <jeeldobariya38@gmail.com>
2023-12-17 09:49:22 +0530

    modify tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,15 @@
 from mocking import MockController
 
 
-def test_welcome_msg(SmartManger, capfd):
-    SmartManger.welcome()
+
+def test_app_exist(SmartManger, capfd):
+    SmartManger.init()
+    SmartManger.main()
     out, _ = capfd.readouterr()
     assert "Welcome to Smart Manager Application" in out
 
 
-def test_app_exist(SmartManger):
-    SmartManger.init()
-    SmartManger.main()
+def test_welcome_msg(SmartManger, capfd):
+    SmartManger.welcome()
+    out, _ = capfd.readouterr()
+    assert "Welcome to Smart Manager Application" in out
