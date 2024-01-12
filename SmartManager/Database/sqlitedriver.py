f80dc14cd3535bdc6728fa6b96547679b85b4bb8
Jeel Dobariya <jeeldobariya38@gmail.com>
2024-01-12 15:05:48 +0530

    add a simple database driver

--- SmartManager/Database/sqlitedriver.py
+++ SmartManager/Database/sqlitedriver.py
@@ -1,2 +1,14 @@
+import sqlite3
+
+
 class SqliteDriver:
-    pass
+    def __init__(self, databasepath):
+        self.database_path = databasepath
+        self.initlize_database()
+    
+    def initlizeddatabase(self):
+        db = sqlite3.connect(self.database_path)
+        db.close()
+    
+    def execute_sql(self, sql_query) -> None:
+        return
